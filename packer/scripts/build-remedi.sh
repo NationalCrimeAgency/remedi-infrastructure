401eb101d61020e67900cee82d37ff1302be6a49
James Baker <james.baker@nccu.nationalcrimeagency.gov.uk>
2018-11-30 10:48:13 +0000

    Update to REMEDI 1.8.2

--- packer/scripts/build-remedi.sh
+++ packer/scripts/build-remedi.sh
@@ -34,7 +34,7 @@ sudo chmod 777 /opt/build
 cd /opt/build
 
 wget https://github.com/ivan-zapreev/Distributed-Translation-Infrastructure/archive/1.8.2.tar.gz
-tar -xzf 1.8.1.tar.gz
+tar -xzf 1.8.2.tar.gz
 mv Distributed-Translation-Infrastructure-1.8.2 Distributed-Translation-Infrastructure
 cd Distributed-Translation-Infrastructure
 
